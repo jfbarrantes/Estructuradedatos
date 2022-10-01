bc830c2e909b6d2817b2fcbe59dd42f5202e3379
jfbarrantes <105684387+jfbarrantes@users.noreply.github.com>
2022-09-30 20:51:11 -0600

    Add files via upload

---
+++ EstructuraDeDatos/Tarea1_Recursividad/src/main/java/com/mycompany/tarea1_recursividad/Recursividad.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea1_recursividad;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Recursividad {
+    //Ejercicio 1 Numero invertido
+   public static int N_Invertido(int n, int i){
+       if(n>0){
+           return N_Invertido(n/10,n%10+i*10);
+       }
+       else{
+           return i;
+       }
+       
+       
+   }
+   
+   //Ejercicio 2 Cadena impresa
+   public static void Cadena_Impresa(String cadena, int i){
+      if(i==cadena.length()-1){
+          System.out.println(cadena.charAt(i));
+          
+      }
+      else{
+          System.out.println(cadena.charAt(i));
+          Cadena_Impresa(cadena,i+1);
+      }
+       
+   }
+   
+   //Ejercicio 3 Llenar vector
+    public static void VECTOR(int []vector, int index, int[] vectornuevo){
+        if(index==vector.length-1){
+            vectornuevo[index]=vector[index];
+            System.out.println(String.valueOf(vectornuevo[index]));
+        }
+        else{
+            vectornuevo[index]=vector[index];
+            System.out.println(String.valueOf(vectornuevo[index]));
+            VECTOR(vector,index+1,vectornuevo);
+        }
+   }
+   
+   //Ejercicio 4 Suma de numeros
+   
+   public static void suma(int [] vector, int index, int suma){
+       
+       if(index==vector.length-1){
+           suma+=vector[index];
+           System.out.println(String.valueOf(suma));
+       }
+       else{
+           suma+=vector[index];
+           suma(vector,index+1,suma);
+       }
+       
+   }
+   
+   //Ejercicio 5 tablas de multiplicar
+   public static String TABLA(int x, int y){
+       
+       if(x==1){
+           return y+" x " + 1 + "="+ y +"\n";
+       }
+       else{
+           int z;
+           z=x;
+           return TABLA(x-1,y)+y+" x "+ z + "=" + (y*z)+"\n";
+       }
+    
+   }
+}
---
+++ EstructuraDeDatos/Tarea1_Recursividad/src/main/java/com/mycompany/tarea1_recursividad/Tarea1_Recursividad.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tarea1_recursividad;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Tarea1_Recursividad {
+
+    public static void main(String[] args) {
+        
+        //Ejercicio 1
+        /*int n=Recursividad.N_Invertido(172,0);
+        System.out.println(n);*/
+        
+        //Ejercicio 2
+        /*Recursividad.Cadena_Impresa("Katherine", 0);
+        */
+        
+         //Ejercicio 3
+        /*
+        int[] vector1={1,2,3,4,5,6};
+        int [] vector2={0,0,0,0,0,0};
+        Recursividad.VECTOR(vector1, 0, vector2);*/
+        
+        
+        //Ejericio 4
+        /*int[] vector={1,2,3,4,5,6};
+        Recursividad.suma(vector, 0, 0);/*
+        
+        //Ejercicio 5
+        /*String tablita;
+        tablita=Recursividad.TABLA(10,7);
+        System.out.println(tablita);*/
+        
+        
+        
+       
+    }
+}
